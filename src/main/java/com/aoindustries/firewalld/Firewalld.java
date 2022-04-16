c7a2fd68b272d5c58f37f49aa52e32d53a39d96d
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:31 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/firewalld/Firewalld.java
+++ src/main/java/com/aoindustries/firewalld/Firewalld.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-firewalld.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.firewalld;
 
 import com.aoapps.collections.AoCollections;
--- src/main/java/com/aoindustries/firewalld/Target.java
+++ src/main/java/com/aoindustries/firewalld/Target.java
@@ -1,6 +1,6 @@
 /*
  * ao-firewalld - Java API for managing firewalld.
- * Copyright (C) 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-firewalld.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.firewalld;
 
 import com.aoapps.lang.NullArgumentException;
